711f4f2abb7815abf96e8fcc55b969c378232c05
Max Kutsepalov <kutsepalov.maks.work@gmail.com>
2021-09-21 20:27:50 +0300

    First commit

---
+++ src/main/java/com/softserve/kutsepalov/game/entity/Warrior.java
@@ -0,0 +1,16 @@
+/**
+ * Copyright 2021
+ *
+ * All rights reserved.
+ *
+ * Created on Sep 21, 2021 8:16:01 PM
+ */
+package com.softserve.kutsepalov.game.entity;
+
+/**
+ * @author Max Kutsepalov
+ *
+ */
+public class Warrior {
+
+}
